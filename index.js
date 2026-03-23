39c1f2979b592f312a5b6306927972becc8717bb
Coovybrasil <nettvfenix@gmail.com>
2026-03-23 10:58:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const http = require('http'); const server = http.createServer((req, res) => { 
-  res.writeHead(200, {'Content-Type': 'text/plain'}); res.end('Coovery TV 
-  funcionando 🚀');
-});
-server.listen(3000, () => { console.log('Servidor rodando na porta 3000');
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("CooveryTV rodando 🚀");
 });
 
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
